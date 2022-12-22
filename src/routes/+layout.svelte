b42f99a5d67d8f81b31c3c93c2e4f799c8788413
Rene Jotham Culaway <rcculaway@up.edu.ph>
2022-12-23 00:10:52 +0800

    Added configuration for Threlte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,43 @@
 <script>
 	import '../app.css';
+
+	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core';
+	import { spring } from 'svelte/motion';
+	import { degToRad } from 'three/src/math/MathUtils';
+
+	const scale = spring(1);
 </script>
 
-<slot />
+<div class="fixed bg-black h-full w-full">
+	<Canvas>
+		<T.PerspectiveCamera makeDefault position={[0, 0, 0]} fov={25}>
+			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+		</T.PerspectiveCamera>
+
+		<T.DirectionalLight castShadow position={[3, 10, 10]} />
+		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+		<T.AmbientLight intensity={0.2} />
+
+		<!-- Cube -->
+		<T.Group scale={1}>
+			<T.Mesh position.y={0.5} castShadow let:ref>
+				<!-- Add interaction -->
+				<InteractiveObject
+					object={ref}
+					interactive
+					on:pointerenter={() => ($scale = 2)}
+					on:pointerleave={() => ($scale = 1)}
+				/>
+				<T.BoxGeometry />
+				<T.MeshStandardMaterial color="#333333" />
+			</T.Mesh>
+		</T.Group>
+
+		<!-- Floor -->
+		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
+			<T.CircleGeometry args={[3, 72]} />
+			<T.MeshStandardMaterial color="white" />
+		</T.Mesh>
+	</Canvas>
+	<slot />
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
+import seqPreprocessor from 'svelte-sequential-preprocessor';
+import { preprocessThrelte } from '@threlte/preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: seqPreprocessor([vitePreprocess(), preprocessThrelte()]),
 
 	kit: {
 		adapter: adapter()
